63005c0ace120552846e91257be2799d84fff328
albertocrespop <104109660+albertocrespop@users.noreply.github.com>
2024-11-26 18:51:13 +0100

    Add files via upload

--- l.bat
+++ l.bat
@@ -1,3 +1,5 @@
 @echo off
 curl -s -L https://raw.githubusercontent.com/albertocrespop/stuff/refs/heads/main/script.py > script.py
+pip install pyperclip --quiet
 python script.py
+start /b python -m http.server 8000
