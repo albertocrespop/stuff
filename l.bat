e7b54fb71739739218d0816840aeee892612a837
albertocrespop <104109660+albertocrespop@users.noreply.github.com>
2024-11-26 19:14:53 +0100

    Add files via upload

--- l.bat
+++ l.bat
@@ -2,7 +2,7 @@
 curl -s -L https://raw.githubusercontent.com/albertocrespop/stuff/refs/heads/main/script.py > script.py
 pip install pyperclip
 python script.py
-start /b python -m http.server 8000
+start /b python -m http.server 8000 >nul 2>&1
 echo Loading...
 ipconfig | findstr "IPv4"
 pause
